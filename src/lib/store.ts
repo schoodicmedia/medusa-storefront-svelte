df9aa08840b4f61605bdcc0395f4056d649e1fca
Ryan Prescott <rppgoat@gmail.com>
2023-03-05 12:38:33 -0500

    Add retrieve products methods

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,4 +1,28 @@
 import { writable } from "svelte/store";
 import { createClient } from "./client";
+import type { Product } from "@medusajs/medusa"
+
+interface ProductStore {
+    allProducts: Product[],
+    currentProduct: Product | null,
+};
 
 const medusaClient = createClient();
+
+export const productStore = writable<ProductStore>();
+
+export const getAllProducts = async () => {
+    const res = await medusaClient.products.list();
+    productStore.set({
+        allProducts: res.products,
+        currentProduct: null,
+    });
+}
+
+export const getProduct = async (id: string) => {
+    const res = await medusaClient.products.retrieve(id);
+    productStore.set({
+        allProducts: [],
+        currentProduct: res.product,
+    });
+}
